b2a677f52b8aba43f1d194fef55b7d31596ff3ff
Naim Zaman <egnaimur@gmail.com>
2021-05-26 00:28:18 +0600

    Dynamic Data Pass Using Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman"]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <Person name={nayoks[0]} nayika="Moushumi"></Person>
+            <Person name="Jasim" nayika="Shabana"></Person>
+            <Person name="BappaRaz" nayika="Cheka"></Person>
+            <Person name="Elias Kanchon" nayika="Bobita"></Person>
       </header>
     </div>
   );
+}
+
+function Person(props){
+    console.log(props);
+    const personStyle={
+        border:'2px solid yellow',
+        borderRadius:'7px',
+        marginBottom:'10px',
+        padding:'10px'
+    }
+  return (
+    <div style={personStyle}>
+      <h1>Name: {props.name} </h1>
+      <h3>Hero of {props.nayika}</h3>
+    </div>
+  )
 }
 
 export default App;
